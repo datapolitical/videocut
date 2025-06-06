a25a7f6c13fa33c86decdc88912d1d138d2ee650
Chris Nicholson <datapolitical@gmail.com>
2025-06-06 02:28:57 -0600

    Refactor utilities into core modules and expand CLI

--- videocut/core/__init__.py
+++ videocut/core/__init__.py
@@ -1,9 +1,11 @@
 """Core video processing utilities package."""
 
-from . import transcription, segmentation, video_editing, nicholson
+from . import transcription, segmentation, video_editing, nicholson, annotation, clip_transcripts
 
 __all__ = [
     "transcription",
+    "annotation",
+    "clip_transcripts",
     "segmentation",
     "video_editing",
     "nicholson",
